5f1583bbe062cc3287a57be888d43fc10484fbb7
DESKTOP-R2NP3JG\user <wayne30317@gmail.com>
2023-02-15 15:18:38 +0800

    components 建立

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import './style.css';
+import Header from './components/header'
+import Section from './components/section'
+import Footer from './components/footer'
 
 function App() {
   return (
     <div>
+      <Header/>
+      <Section/>
+      <Footer/>
     </div>
   );
 }
